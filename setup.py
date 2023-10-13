1185308ecf868c7eb75a673cc9024ab94deb434e
tlinden tlinden <thomaz.linden@gmail.com>
2023-10-13 09:53:59 +0000

    Create pixel_art package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='pixel_art',
+  version='0.1',
+  description='Pckage for drawing pixel art to the terminal.',
+  author='Thomas',
+  packages=['pixel_art'],
+  install_requires=['colorama']
+)
